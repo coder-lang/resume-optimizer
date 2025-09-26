ca133f39ef16c5e265397de8076927c9c61a627d
coder_lang <ankursrivastava98@gmail.com>
2025-09-26 21:50:29 +0530

    port change for render

--- app.py
+++ app.py
@@ -389,4 +389,5 @@ Job: {job_desc}"""
                                 total=total)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
